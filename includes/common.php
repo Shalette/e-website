8ca57ab71bfb877324544b1b2fc534d12fd952f5
Shalette <47327883+Shalette@users.noreply.github.com>
2019-02-05 20:41:18 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "root", "s1297n1297", "selectiall") or die(mysqli_error($con));
+$con = mysqli_connect("localhost", "root", "", "selectiall") or die(mysqli_error($con));
 session_start();
 ?>
